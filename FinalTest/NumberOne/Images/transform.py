94cdce67fe5f4fc37e89ccda6e5bcde723ff245d
Natalia Cardona <natathemilk@gmail.com>
2024-06-23 16:05:21 -0500

    WIP number 1

--- FinalTest/NumberOne/Images/transform.py
+++ FinalTest/NumberOne/Images/transform.py
@@ -1,14 +1,15 @@
+from pyspark.sql import HiveContext
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, when
+from pyspark.sql.functions import col, when, to_date
 
-# Crear una SparkSession
-spark = SparkSession.builder \
-    .appName("CSV to DataFrame") \
-    .getOrCreate()
+sc = SparkContext('local')
+spark = SparkSession(sc)
+hc = HiveContext(sc)
 
-file_2021 = f'hdfs://172.17.0.2:9000/ingest/2021-informe-ministerio.csv'
-file_2022 = f'hdfs://172.17.0.2:9000/ingest/202206-informe-ministerio.csv'
-file_details = f'hdfs://172.17.0.2:9000/ingest/aeropuertos_detalle.csv'
+file_2021 = 'hdfs://172.17.0.2:9000/ingest/2021-informe-ministerio.csv'
+file_2022 = 'hdfs://172.17.0.2:9000/ingest/202206-informe-ministerio.csv'
+file_details = 'hdfs://172.17.0.2:9000/ingest/aeropuertos_detalle.csv'
 
 # Leer los archivos CSV especificando el delimitador
 df_2021 = spark.read.option("delimiter", ";").option("header", "true").csv(file_2021)
@@ -30,7 +31,7 @@ df_union = df_2021.unionByName(df_2022)
 columns_to_drop = ["inhab", "fir", "Calidad dato"]
 df_union = df_union.drop(*columns_to_drop)
 
-# Filtrar los vuelos internacionales (asumiendo que hay una columna 'Tipo de Movimiento')
+# Filtrar los vuelos domésticos (asumiendo que hay una columna 'Clasificación Vuelo')
 df_domestic = df_union.filter(col('Clasificación Vuelo') == 'Doméstico')
 
 # Convertir valores NULL en 0 en las columnas 'Pasajeros' y 'distancia_ref'
@@ -45,5 +46,43 @@ df_domestic.printSchema()
 print("Primeros 5 registros del DataFrame transformado:")
 df_domestic.show(5)
 
-# Detener la SparkSession
-spark.stop()
+# Renombrar columnas para que coincidan con la estructura del schema destino
+df_domestic = df_domestic.withColumnRenamed('Fecha', 'fecha') \
+                         .withColumnRenamed('Hora UTC', 'horaUTC') \
+                         .withColumnRenamed('Clase de Vuelo (todos los vuelos)', 'clase_de_vuelo') \
+                         .withColumnRenamed('Clasificación Vuelo', 'clasificacion_de_vuelo') \
+                         .withColumnRenamed('Tipo de Movimiento', 'tipo_de_movimiento') \
+                         .withColumnRenamed('Aeropuerto', 'aeropuerto') \
+                         .withColumnRenamed('Origen / Destino', 'origen_destino') \
+                         .withColumnRenamed('Aerolinea Nombre', 'aerolinea_nombre') \
+                         .withColumnRenamed('Aeronave', 'aeronave') \
+                         .withColumnRenamed('Pasajeros', 'pasajeros')
+
+# Convertir la columna 'fecha' en un tipo de dato date
+df_domestic = df_domestic.withColumn('fecha', to_date(col('fecha'), 'dd/MM/yyyy'))
+
+# Seleccionar y castear las columnas necesarias
+df_domestic = df_domestic.select(
+    col('fecha'),
+    col('horaUTC').cast("string"),
+    col('clase_de_vuelo').cast("string"),
+    col('clasificacion_de_vuelo').cast("string"),
+    col('tipo_de_movimiento').cast("string"),
+    col('aeropuerto').cast("string"),
+    col('origen_destino').cast("string"),
+    col('aerolinea_nombre').cast("string"),
+    col('aeronave').cast("string"),
+    col('pasajeros').cast("int")
+)
+
+# Mostrar tipos de datos de las columnas
+df_domestic.printSchema()
+
+# Mostrar los primeros 5 registros del DataFrame transformado
+df_domestic.show(5)
+
+# Crear una vista temporal
+df_domestic.createOrReplaceTempView("tmp_flights_data")
+ 
+hc.sql("insert into fligthsdb.vuelos select * from tmp_flights_data")
+
